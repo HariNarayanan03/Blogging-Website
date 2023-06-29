9d9ac720ec4bbbc76c1a41a8c158af9970f9516c
Hari Narayanan R <83228517+HariNarayanan03@users.noreply.github.com>
2023-06-29 17:38:55 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,12 +1,12 @@
 
 //  const admin = require('firebase-admin');
   const firebaseConfig = {
-    apiKey: "AIzaSyCgJoRjz_SDp8iOVi8_2pEB_YEAjE2tJug",
-    authDomain: "blog-website-ac113.firebaseapp.com",
-    projectId: "blog-website-ac113",
-    storageBucket: "blog-website-ac113.appspot.com",
-    messagingSenderId: "1064174485539",
-    appId: "1:1064174485539:web:08772022ce591b6dd83196"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   // Initialize Firebase
